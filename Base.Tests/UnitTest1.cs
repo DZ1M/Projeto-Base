156dc4d35262d93a32cf248b9c6d054508dfcb3d
Richard <riguikage@gmail.com>
2021-03-10 00:15:36 -0300

    Adicionando Bibliotecas Bases e Web.

---
+++ Base.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Base.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

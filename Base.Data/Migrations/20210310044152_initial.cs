b3eeac02dcbb249c9cce9ac101f5e0398f5c7e5e
Richard <riguikage@gmail.com>
2021-03-10 01:46:03 -0300

    Adicionando Migrations base

---
+++ Base.Data/Migrations/20210310044152_initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Base.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v1()"),
+                    email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    senha = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    IdEmpresa = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "usuarios",
+                schema: "public");
+        }
+    }
+}
